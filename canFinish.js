c6f5a3f90045b1d31dab8077009b319428fd6c93
zhangyong <zhangyong_web@163.com>
2022-02-28 20:16:52 +0800

    canFinish

--- canFinish.js
+++ canFinish.js
@@ -6,6 +6,39 @@
  * @param {number[][]} prerequisites
  * @return {boolean}
  */
- var canFinish = function(numCourses, prerequisites) {
-
+var canFinish = function(numCourses, prerequisites) {
+  let inDegree = new Array(numCourses)
+  inDegree.fill(0)
+  let map = {}
+  for (let i = 0; i < prerequisites.length; i++) {
+    let item = prerequisites[i]
+    inDegree[item[0]] += 1
+    if (!map[item[1]]) {
+      map[item[1]] = [item[0]]
+    } else {
+      map[item[1]].push(item[0])
+    }
+  }
+  let queue = []
+  for (let i = 0; i < inDegree.length; i++) {
+    const item = inDegree[i];
+    if (item === 0) {
+      queue.push(i)
+    }
+  }
+  while (queue.length) {
+    let item = queue.shift()
+    numCourses--
+    let curList = map[item]
+    if(curList) {
+      for (let i = 0; i < curList.length; i++) {
+        const element = curList[i];
+        inDegree[element]--
+        if (inDegree[element] === 0) {
+          queue.push(element)
+        }
+      }
+    }
+  }
+  return numCourses === 0
 };
